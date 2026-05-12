7486d143035ad2789c7447b85e636296f75b300f
criscrafter <95765057+criscrafter@users.noreply.github.com>
2026-05-12 15:36:56 +0200

    Update Anzeigemodul.cpp

--- Anzeigemodul.cpp
+++ Anzeigemodul.cpp
@@ -6,18 +6,16 @@
 #include <Adafruit_SSD1306.h>
 
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_I2C_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define SCREEN_I2C_ADDRESS 0x3C ///< See datasheet for Address;
 
-unsigned long lastDisplayUpdate = millis();
-
+unsigned long lastDisplayUpdate = 0;
+unsigned long lastReceivedMessage = 0;
 
 MCP2515 mcp2515(8);
 
 Adafruit_SSD1306 OLED(128, 64, &Wire, OLED_RESET);
 
-
 struct can_frame canMsg;
-
 
 float Motor1Temp = 0;
 float Motor2Temp = 0;
@@ -25,6 +23,8 @@ float ZinnbadTemp = 0;
 
 
 void setup() { 
+
+  Serial.begin(921600);
 
   if(!OLED.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
@@ -37,11 +37,12 @@ void setup() {
   mcp2515.setNormalMode();
 
   OLED.clearDisplay();
-
   OLED.setTextSize(1);                         // Schriftgröße auf 1 setzen
   OLED.setTextColor(SSD1306_WHITE);            // Schriftfarbe auf Weiß setzen
   OLED.setCursor(0,0);                         // Cursor zu Positiion 0,0 bewegen
-  OLED.println(F("CAN Data Display"));
+  OLED.println(F("ZQ - Programmierung"));
+  OLED.println("Datenübertragung");
+  OLED.println("über CAN Bus");
   OLED.println("- Friedrich Rosenthal");
   OLED.display();
 
@@ -52,46 +53,68 @@ void loop() {
 
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
 
-    if (canMsg.can_id == 0x000 && canMsg.can_dlc >= 2) {
-      int16_t scaledEngOil = (canMsg.data[0] << 8) | canMsg.data[1];
-      engOilTemp = (scaledEngOil / 100.0) - 60.0;
+    if (canMsg.can_id == 0x101 && canMsg.can_dlc >= 4) {
+      uint16_t ScaledMotor1Temp = (canMsg.data[0] << 8) | canMsg.data[1];
+      Motor1Temp = (ScaledMotor1Temp / 100.0) - 200.0;
+
+      uint16_t ScaledMotor2Temp = (canMsg.data[2] << 8) | canMsg.data[3];
+      Motor2Temp = (ScaledMotor2Temp / 100.0) - 200.0;
     }
 
-    if (canMsg.can_id == 0x002 && canMsg.can_dlc >= 2) {
-      int16_t scaledPressure = (canMsg.data[0] << 8) | canMsg.data[1];
-      engOilPressure = (scaledPressure / 1000.0);
+    if (canMsg.can_id == 0x102 && canMsg.can_dlc >= 2) {
+      uint16_t ScaledZinnbadTemp = (canMsg.data[0] << 8) | canMsg.data[1];
+      ZinnbadTemp = (ScaledZinnbadTemp / 100.0) - 200.0;
     }
+
+    lastReceivedMessage = millis();
 
   }
 
   if((millis() - lastDisplayUpdate) > 100){
     lastDisplayUpdate = millis();
+
     OLED.clearDisplay();
 
-    OLED.setCursor(0,0);
-    OLED.print("Wassertemp:");
-    OLED.print(engCoolantTemp, 2);
-    OLED.print("C");
+    if(millis() - lastReceivedMessage > 1000) {
+      OLED.setCursor(0,0);
+      OLED.print("KEIN SIGNAL");
+    } else {
+    
+      OLED.setCursor(0,0);
+      OLED.print("Motor 1:");
 
-    OLED.setCursor(0,10);
-    OLED.print("Motoroel.:");
-    OLED.print(engOilTemp, 2);
-    OLED.print("C");
+      if(Motor1Temp == -200){
+        OLED.print(" ERROR");
+      } else {
+        OLED.print(Motor1Temp, 1);
+        OLED.print("C");
+      }
 
-    OLED.setCursor(0,20);
-    OLED.print("Transoel.:");
-    OLED.print(transOilTemp, 2);
-    OLED.print("C");
 
-    OLED.setCursor(0,30);
-    OLED.print("Oeldruck:");
-    OLED.print(engOilPressure, 3);
-    OLED.print("Bar");
+      OLED.setCursor(0,10);
+      OLED.print("Motor 2:");
 
-    OLED.setCursor(0,40);
-    OLED.print("Luefter PWM:");
-    OLED.print(fanPWM);    
+      if(Motor2Temp == -200){
+        OLED.print(" ERROR");
+      } else {
+        OLED.print(Motor2Temp, 1);
+        OLED.print("C");
+      }
+
+
+      OLED.setCursor(0,20);
+      OLED.print("Zinnbad:");
+
+      if(ZinnbadTemp == -200){
+        OLED.print(" ERROR");
+      } else {
+        OLED.print(ZinnbadTemp, 1);
+        OLED.print("C");
+      }
+
+    }
 
     OLED.display();
   }
+
 }
